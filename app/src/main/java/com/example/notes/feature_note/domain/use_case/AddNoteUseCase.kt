c5408ceff5def20a20b1a0ddfc06e0ef6353726b
YashKr01 <yashkumar201301@gmail.com>
2021-12-20 22:08:54 +0530

    Notes ViewModel

---
+++ app/src/main/java/com/example/notes/feature_note/domain/use_case/AddNoteUseCase.kt
@@ -0,0 +1,17 @@
+package com.example.notes.feature_note.domain.use_case
+
+import com.example.notes.feature_note.domain.model.InvalidNoteException
+import com.example.notes.feature_note.domain.model.Note
+import com.example.notes.feature_note.domain.repository.NoteRepository
+
+class AddNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if (note.title.isNotBlank()) throw InvalidNoteException("Empty Title!!")
+        if(note.content.isNotBlank()) throw InvalidNoteException("Empty Content!!")
+        repository.insertNote(note)
+    }
+}
--- app/src/main/java/com/example/notes/feature_note/domain/use_case/NotesUseCases.kt
+++ app/src/main/java/com/example/notes/feature_note/domain/use_case/NotesUseCases.kt
@@ -2,5 +2,6 @@ package com.example.notes.feature_note.domain.use_case
 
 data class NotesUseCases(
     val getNotesUseCase: GetNotesUseCase,
-    val deleteNoteUseCase: DeleteNoteUseCase
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase:AddNoteUseCase
 )
